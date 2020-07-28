57d3c0d8fa3d67cc9111da194b14c16b05487ada
klemminn <klemenz88@gmail.com>
2020-07-28 23:44:40 +0000

    Updates

---
+++ precache-manifest.c9fbfb03d3e4ad86ae929d729c4bb98b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f132080854802c4c1b0315ccf0f5763e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ebe27397c5314d7a541",
+    "url": "/static/css/2.75d792e8.chunk.css"
+  },
+  {
+    "revision": "34599599f32640d1baf6",
+    "url": "/static/css/main.c76b8dd6.chunk.css"
+  },
+  {
+    "revision": "2ebe27397c5314d7a541",
+    "url": "/static/js/2.2e88aa82.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2e88aa82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34599599f32640d1baf6",
+    "url": "/static/js/main.bedf78b1.chunk.js"
+  },
+  {
+    "revision": "09c429e04e6ad71a3b18",
+    "url": "/static/js/runtime-main.d18f532a.js"
+  }
+]);
